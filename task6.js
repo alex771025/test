303cd1e540540c43711b3bb4b7e16f9c759dd878
alex771025 <alex771025azin@abv.bg>
2025-01-17 14:59:10 +0200

    Print each element in array

---
+++ task6.js
@@ -0,0 +1,9 @@
+//Print Each Element in an Array
+//Goal: Loop through an array and print each item.
+
+//Task: Create an array of 5 favorite fruits and use a for loop to print each fruit.
+
+const favoriteFruits =['Apple', 'Banana', 'Cherry', 'Date', 'ElderBerry'];
+for(let i = 0; i < favoriteFruits.length; i++){
+    console.log(favoriteFruits[i]);
+}
