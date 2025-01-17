d26117128548a3a5ecb69cbb1d2f767f71f35c8e
alex771025 <alex771025azin@abv.bg>
2025-01-17 14:09:41 +0200

    test

---
+++ task2.js
@@ -0,0 +1,12 @@
+//Sum of Numbers 1 to 10
+   // Goal: Understand how to accumulate values inside a loop.
+
+//Task: Write a program that calculates the sum of numbers from 1 to 10 using a for loop and prints the result.
+
+let sum = 0; 
+
+for (let i = 1; i <= 10; i++) {
+    sum += i; 
+}
+
+console.log(sum); 
