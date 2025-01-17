873795d9d0b7c3079dd64b2420747811cfbd60b9
alex771025 <alex771025azin@abv.bg>
2025-01-17 14:49:09 +0200

    create a simple countdown timer

---
+++ task5.js
@@ -0,0 +1,8 @@
+//Create a Simple Countdown Timer
+//Goal: Use the for loop to create a countdown from 10 to 1.
+
+//Task: Write a program that counts down from 10 to 1 and prints "Liftoff!" when the countdown reaches 0.
+for(let i = 10; i > 0; i --){
+    console.log(i);
+}
+console.log("LiftOff");
