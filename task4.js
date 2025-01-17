7b85df0ec855c9e379c20b8224078797af8948e9
alex771025 <alex771025azin@abv.bg>
2025-01-17 14:35:28 +0200

    Even numbers from 1 to 20

---
+++ task4.js
@@ -0,0 +1,11 @@
+//Even Numbers from 1 to 20
+//Goal: Loop through a range of numbers and filter even numbers.
+
+//Task: Write a program that prints all the even numbers between 1 and 20.
+
+//Hint: Use an if statement inside the loop to check if a number is even (i % 2 === 0).
+for(let i = 1; i<= 20; i++){
+    if(i % 2 ===0){
+        console.log(i);
+    }
+}
