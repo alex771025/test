5cb58719ecb8dd273aa36fe52bc08cac9d9a796b
alex771025 <alex771025azin@abv.bg>
2025-01-17 17:51:52 +0200

    Task: Count from 1 to 5 (using while loop)

---
+++ task10.js
@@ -0,0 +1,11 @@
+//Task: Count from 1 to 5 (Using while loop)
+//Goal: Use a while loop to count from 1 to 5 and print each number.
+
+///ask: Write a program that uses a while loop to print the numbers from 1 to 5, one on each line.
+
+//Hint: Initialize a variable (e.g., i) to 1, then keep increasing it by 1 on each iteration of the while loop, until it reaches 5.
+let i = 1;
+while(i <= 5){
+    console.log(i);
+    i++;
+}
